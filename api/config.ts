aea7c7d7b892b7b328cb51aa15407123de4ebd48
farhanmian <farhanmian099@gmail.com>
2022-08-11 11:19:00 +0530

    fix a bug

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api";
-// export const BASE_URL = "https://offshoreserver.herokuapp.com/api";
+// export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://offshoreserver.herokuapp.com/api";
 
 export const URLS = {
   SIGN_IN: BASE_URL + "/user/login",
